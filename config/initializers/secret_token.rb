e52813d004aff5626b1f3b689bcb48a94049b24c
Austin Roberts <silver2002ws6@yahoo.com>
2013-08-09 14:12:17 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arrails::Application.config.secret_token = '17435b3bed95bf586dd1352beabf481c58f258bbdcfc44373362dd7bc97c291ba3c9a3ca718cae71d9036c33ec26003025e6d17fff1945989cbe67406bdab5da'
