1849320d6fb284ea0287f0e02b40bc478ac475c1
Austin Roberts <silver2002ws6@yahoo.com>
2013-08-23 12:05:27 -0400

    Devise Install User Database Creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Arrails::Application.routes.draw do
+  devise_for :users
+
+  devise_for :installs
+
   get 'about' => 'pages#about'
   get 'information' => 'pages#information'
 
@@ -62,4 +66,5 @@ Arrails::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+  #devise_for :installs
 end
